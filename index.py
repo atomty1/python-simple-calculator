4ba01cda6e98b7733a5382e86159d1b2d21dddc8
Taye Abidakun <tayeabidakun@gmail.com>
2025-01-22 20:26:51 +0100

    multiplication added to the calculator

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 def calculator():
     print("Calculator")
-    user_operation = input("1. + 2. - : ")
+    user_operation = input("1. + 2. - 3. * : ")
     if(user_operation == "1"):
         addition()
     elif(user_operation == "2"):
         subtraction()
+    elif(user_operation == "3"):
+        multiplication()
 
 def addition():
     first_num = float(input("Enter the first number: "))
@@ -19,6 +21,8 @@ def subtraction():
     print(f"{first_num} - {second_num} = {result}")
 
 def multiplication():
-    first_num
-
+    first_num = float(input("Enter the first number: "))
+    second_num = float(input("Enter the second number: "))
+    result = first_num * second_num
+    print(f"{first_num} * {second_num} = {result}")
 calculator()
