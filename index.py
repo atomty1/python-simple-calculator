cc6998e596b9c03b754af9594510d9b177d287dc
Taye Abidakun <tayeabidakun@gmail.com>
2025-01-22 20:29:29 +0100

    division function added

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 def calculator():
     print("Calculator")
-    user_operation = input("1. + 2. - 3. * : ")
+    user_operation = input("1. + 2. - 3. * 4. /: ")
     if(user_operation == "1"):
         addition()
     elif(user_operation == "2"):
         subtraction()
     elif(user_operation == "3"):
         multiplication()
+    elif(user_operation == "4"):
+        division()
 
 def addition():
     first_num = float(input("Enter the first number: "))
@@ -25,4 +27,11 @@ def multiplication():
     second_num = float(input("Enter the second number: "))
     result = first_num * second_num
     print(f"{first_num} * {second_num} = {result}")
+
+def division():
+    first_num = float(input("Enter the first number: "))
+    second_num = float(input("Enter the second number: "))
+    result = first_num / second_num
+    print(f"{first_num} / {second_num} = {result}")
+
 calculator()
