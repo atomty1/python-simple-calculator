d500d084a3ff2d3db23ad2ec3125a9699287aca7
Taye Abidakun <tayeabidakun@gmail.com>
2025-01-22 20:16:13 +0100

    addition and subtraction functions added

---
+++ index.py
@@ -0,0 +1,21 @@
+def calculator():
+    print("Calculator")
+    user_operation = input("1. + 2. - : ")
+    if(user_operation == "1"):
+        addition()
+    elif(user_operation == "2"):
+        subtraction()
+
+def addition():
+    first_num = float(input("Enter the first number: "))
+    second_num = float(input("Enter the second number: "))
+    result = first_num + second_num
+    print(f"{first_num} + {second_num} = {result}")
+
+def subtraction():
+    first_num = float(input("Enter the first number: "))
+    second_num = float(input("Enter the second number: "))
+    result = first_num - second_num
+    print(f"{first_num} - {second_num} = {result}")
+
+calculator()
